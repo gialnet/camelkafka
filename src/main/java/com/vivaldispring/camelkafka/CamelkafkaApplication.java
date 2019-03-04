da567ef3d580c656f00e782a3c035f0c8e2fd229
Antonio <antonio.gialnet@gmail.com>
2019-03-04 18:35:34 +0100

    first commit

---
+++ src/main/java/com/vivaldispring/camelkafka/CamelkafkaApplication.java
@@ -0,0 +1,13 @@
+package com.vivaldispring.camelkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelkafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamelkafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vivaldispring/camelkafka/CamelkafkaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vivaldispring.camelkafka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CamelkafkaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
